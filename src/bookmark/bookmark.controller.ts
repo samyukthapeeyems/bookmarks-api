05b13524c23cd8b82449d4dbfa1e31682db4551b
samyukthapeeyems <samyukthapeeyems@gmail.com>
2023-02-12 19:27:22 +0530

    added class validator for bookmark dtos, implemented bookmark services and controllers

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,22 +1,54 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('bookmark')
 export class BookmarkController {
     constructor(private bookmarkService: BookmarkService){}
 
     @Get()
-    getBookmarks(){}
+    getBookmarks(@GetUser('id') userId: number){
+            return this.bookmarkService.getBookmarks(userId);
+        }
+
 
     @Get(':id')
-    getBookmarkById(){}
+    getBookmarkById(
+        @GetUser('id') userId: number,
+        @Param('id', ParseIntPipe) bookmarkId: number
+    ){
+        return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+    }
+
 
     @Post()
-    createBookmark(){}
+    createBookmark(
+        @GetUser('id') userId: number,
+        @Body() dto: CreateBookmarkDto
+        ){
+            return this.bookmarkService.createBookmark(userId, dto);
+        }
 
+        
     @Patch(':id')
-    editBookmarkById(){}
+    editBookmarkById(
+        @GetUser('id') userId: number,
+        @Param('id', ParseIntPipe) bookmarkId: number,
+        @Body() dto: EditBookmarkDto
+    ){
+        return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+    }
 
+
+    @HttpCode(HttpStatus.NO_CONTENT)
     @Delete(':id')
-    deleteBookmark(){}
+    deleteBookmark(
+        @GetUser('id') userId: number,
+        @Param('id', ParseIntPipe) bookmarkId: number
+    ){
+        return this.bookmarkService.deleteBookmark(userId, bookmarkId);
+    }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateBookmarkDto } from './dto';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
 export class BookmarkService {
@@ -25,6 +25,7 @@ export class BookmarkService {
         });
     }
 
+
     async createBookmark(userId: number, dto: CreateBookmarkDto){
         const bookmark = await this.prisma.bookmark.create({
             data : {
@@ -36,7 +37,50 @@ export class BookmarkService {
         return bookmark;
     }
 
-    editBookmarkById(){}
 
-    deleteBookmark(){}
+    async editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto){
+
+        //get bookmark by id
+        const bookmark = await this.prisma.bookmark.findUnique({
+            where : {
+                id: bookmarkId
+            }
+        });
+
+        //check if user owns the bookmark
+        if(!bookmark || bookmark.userId !== userId){
+            throw new ForbiddenException('Access to the resources denied');
+        }
+
+        //return the updated bookmark
+        return this.prisma.bookmark.update({
+            where : {
+                id: bookmarkId
+            },
+            data : {
+                ...dto
+            }
+        })
+    }
+
+    async deleteBookmark(userId: number, bookmarkId: number){
+
+        //get bookmark by id
+        const bookmark = await this.prisma.bookmark.findUnique({
+            where : {
+                id: bookmarkId
+            }
+        });
+
+        //check if user owns the bookmark
+        if(!bookmark || bookmark.userId !== userId){
+            throw new ForbiddenException('Access to the resources denied');
+        }
+
+        await this.prisma.bookmark.delete({
+            where : {
+                id: bookmarkId
+            }
+        })
+    }
 }
